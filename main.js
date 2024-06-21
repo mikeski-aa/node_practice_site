7f6817805b5d68ff5c7e3f8813833dde316a718e
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-06-21 14:39:58 +0200

    Creating simple node app

---
+++ main.js
@@ -0,0 +1,9 @@
+let http = require("http");
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("Testing creating node app");
+    res.end();
+  })
+  .listen(8080);
