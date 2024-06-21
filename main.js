f51088ddd9f773ef6bceb7f096064a34f70aaf6d
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-06-21 16:14:10 +0200

    working on this

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 let http = require("http");
+let url = require("url");
+let fs = require("fs");
 
 http
   .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("Testing creating node app");
-    res.end();
+    let q = url.parse(req.url, true);
+    let filename = "./pages/" + q.pathname;
+
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404 Not Found");
+      }
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
   })
   .listen(8080);
