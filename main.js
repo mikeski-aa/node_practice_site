b5135fe6321837c52a110a78bfb1dd50d4798403
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-06-22 11:51:01 +0200

    Working on showing correct sites on launch

--- main.js
+++ main.js
@@ -7,14 +7,23 @@ http
     let q = url.parse(req.url, true);
     let filename = "./pages/" + q.pathname;
 
-    fs.readFile(filename, function (err, data) {
-      if (err) {
+    if (q.pathname === "/") {
+      console.log("default detected");
+      fs.readFile("./pages/index.html", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else {
+      fs.readFile(filename, function (err, data) {
+        if (err) {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          return res.end("404 Not Found");
+        }
         res.writeHead(404, { "Content-Type": "text/html" });
-        return res.end("404 Not Found");
-      }
-      res.writeHead(404, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
+        res.write(data);
+        return res.end();
+      });
+    }
   })
   .listen(8080);
